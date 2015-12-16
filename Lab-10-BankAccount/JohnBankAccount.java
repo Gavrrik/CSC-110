0d63a5033a20d62c37b980a3a1bd17d1812d89b8
HalcyonFox <Espinoza.Gabriel@outlook.com>
2015-12-16 10:49:58 -0500

    JohnBankAccount
    This Class contains the methods for JohnBankAccount.

---
+++ Lab-10-BankAccount/JohnBankAccount.java
@@ -0,0 +1,22 @@
+
+public class JohnBankAccount {
+    private double balance;
+
+    public void deposit (double amount)
+    {
+
+    balance += 0 + amount;
+    
+    }
+    public void withdraw (double amount)
+    {
+
+    balance -= 0 - amount;
+    
+    } 
+    public double getBalance()
+    {
+    return balance;
+    }
+
+}
