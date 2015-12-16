996d6c7852fe5bdb4c3ded75ee781a34c3815b70
HalcyonFox <Espinoza.Gabriel@outlook.com>
2015-12-16 10:45:26 -0500

    BillBankAccount
    This Class contains the methods for BillBankAccount.

---
+++ Lab-10-BankAccount/BillBankAccount.java
@@ -0,0 +1,22 @@
+
+public class BillBankAccount {
+    private double balance;
+
+    public void deposit (double amount)
+    {
+
+    balance += 1000 + amount;
+    
+    }
+    public void withdraw (double amount)
+    {
+
+    balance -= 1000 - amount;
+    
+    } 
+    public double getBalance()
+    {
+    return balance;
+    }
+
+}
