2c4e5f288b743cfb1170577ad36a4daaf6a15fee
HalcyonFox <Espinoza.Gabriel@outlook.com>
2015-12-16 10:46:49 -0500

    JaneBankAccount
    This Class contains the methods for JaneBankAccount.

---
+++ Lab-10-BankAccount/JaneBankAccount.java
@@ -0,0 +1,22 @@
+
+public class JaneBankAccount {
+    private double balance;
+
+    public void deposit (double amount)
+    {
+
+    balance += 500 + amount;
+    
+    }
+    public void withdraw (double amount)
+    {
+
+    balance -= 500 - amount;
+    
+    } 
+    public double getBalance()
+    {
+    return balance;
+    }
+
+}
