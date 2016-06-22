c8e5477d6643dc132e36e40ee6cf8bb88a3569d5
Gabriel <Espinoza.Gabriel@outlook.com>
2016-06-22 10:51:32 -0400

    Add files via upload

---
+++ FinalExam/VotingAge.java
@@ -0,0 +1,31 @@
+
+
+public class VotingAge
+{ 
+   public static void main(String[] args) 
+   { 
+     
+	  int myage=15;
+	  String abletovote ="yes";
+	  int votingage=18;
+	 
+
+		 if (myage < votingage)
+			 
+			 {
+				 System.out.println("My Age:" + myage);
+				 System.out.println("Able to Vote: NO");
+			 }
+
+		 else 
+			 
+			 {
+				 System.out.println("My Age:" + myage);
+				 System.out.println("Able to Vote:" + abletovote);
+			 }
+		 
+
+   }
+   
+
+}
