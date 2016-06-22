f8fcd5d8da1c3a73668fcbd8780f63656b823d8a
Gabriel <Espinoza.Gabriel@outlook.com>
2016-06-22 11:08:05 -0400

    Add files via upload

---
+++ YoungOrOld.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+/** 
+ * @developed by Halcyon 
+*/ 
+
+public class YoungOrOld 
+{ 
+   public static void main(String[] args) 
+   { 
+     
+	  int age;
+	  
+	     //Create a Scanner object for keyboard input.
+		 Scanner keyboard = new Scanner (System.in);
+		 
+		 //Get a person's age
+		 System.out.print("Enter your age:");
+		 age = keyboard.nextInt();
+		 
+		 //is the person young or old.
+		 if (age > 30)
+			 
+			 {
+				 System.out.println("Sorry Your Old!");
+				 System.out.println(":(");
+				 
+		}
+        else
+        {
+			System.out.println("You're very young and you have a whole future ahead, WorkHard!.");
+				
+				
+		}
+		 keyboard.close();	 
+   }
+   
+   
+}
