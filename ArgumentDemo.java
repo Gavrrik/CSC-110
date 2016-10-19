a6bf475202b4d12ffb1c0b37c04fca40ee1f1513
Gabriel Espinoza <Halcyon@Gabriels-iMac.nyc.rr.com>
2016-10-19 10:20:32 -0400

    fixed and reformatted code style

--- ArgumentDemo.java
+++ ArgumentDemo.java
@@ -1,16 +1,14 @@
 
-public class ArgumentDemo
-{
-    public static void main(String[] args)   
-     {
+public class ArgumentDemo {
+
+    public static void main(String[] args) {
     	showKilometers (10);
     	showKilometers (100);
     	showKilometers (1000);
      }
-    public static void showKilometers (double miles)
-    {
+
+    public static void showKilometers (double miles) {
     	double Kilometers = miles * 1.609;
     	System.out.println(miles + " miles is equal to " + Kilometers + " Kilometers.");
     }
-    }
-    
+  }
